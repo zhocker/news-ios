6b8c892fb136feb444a8cedb1d019e02ab5490c2
Tanawat Polsuwan <numpun@gmail.com>
2024-05-30 20:35:33 +0700

    create project

---
+++ news-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-ios
+//
+//  Created by User on 30/5/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
